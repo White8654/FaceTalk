5ade00cf933d7444eef5cce1f59db55bb62c81b9
White8654 <sohanmahanta16@gmail.com>
2023-07-17 20:52:18 +0530

    About api added

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     // credentials: true,
   },
 });
